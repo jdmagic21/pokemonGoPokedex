dc23757c06b445f83d72cc30d9f599da7408faff
jutt <jutt@iupui.edu>
2020-05-20 23:02:52 -0400

    Sort by property and update remaining distance

--- server/index.js
+++ server/index.js
@@ -6,27 +6,39 @@ mongoose.connect(jsonSettings.connectionString,
 var db = mongoose.connection;
 const pokeDex = require('../Models/index').pokeDex;
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const port = 3000;
 
-app.get('/pokemon/', async (req, res) =>
+app.get('/pokemon', async (req, res) =>
 {
-    const all = await pokeDex.find({},
-        {
-            idNumber: 1,
-            name: 1,
-            km: 1,
-            miles: 1,
-            needed: 1,
-            evolutionCost: 1,
-            evolvesInto: 1,
-            candyCount: 1,
-            threeStars: 1
-        }).sort({ name: 1 }).exec();
-    res.json(all);
+    const filter={
+       __v: 0
+    }
+
+    if(req.query.sort == null){
+        const all = await pokeDex.find({}, filter).sort({ name: 1 }).exec();
+        res.json(all);
+    }
+    else{
+        var sort = {};       
+        if(req.query.order === "desc"){
+            sort[req.query.sort] = -1;
+        }
+        else if(req.query.order === "asc"){
+            sort[req.query.sort] = 1;
+        }       
+
+        const sortall = await pokeDex.find({}, filter).sort(sort).exec(); 
+        res.json(sortall);
+    }   
 });
+
+
+
 app.get('/pokemon/:id', async (req, res) =>
 {
     const singlePoke = await pokeDex.findById(req.params.id).exec();
@@ -58,7 +70,21 @@ app.post('/pokemon/update', async (req, res) =>
         poke.candyCount = req.body.candyCount || poke.candyCount;
         poke.threeStars = req.body.threeStars || poke.threeStars;
         poke.needed = req.body.needed || poke.needed; 
-        poke.candyRemaining = poke.evolutionCost - poke.candyCount; 
+        const candyRemaining = poke.evolutionCost - poke.candyCount; 
+        if(candyRemaining > 0){
+            poke.candyRemaining = candyRemaining; 
+            poke.kmsRemaining = candyRemaining * poke.kms; 
+            poke.milesRemaining = candyRemaining * poke.miles;
+        }
+        else
+        {
+            poke.candyRemaining = 0; 
+            if(poke.evolutionCost != null){
+                poke.kmsRemaining =  poke.evolutionCost * poke.kms; 
+                poke.milesRemaining = (poke.evolutionCost * poke.miles).toFixed(2); 
+            }     
+        }        
+           
         await poke.save();
         res.json(poke);
     }
