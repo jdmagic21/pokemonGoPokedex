07bf8f6dcc5a3fd20550009eb14e029dd6aa4e06
jutt <jutt@iupui.edu>
2020-05-20 23:50:43 -0400

    data table formatting

--- src/components/PokemonTable.js
+++ src/components/PokemonTable.js
@@ -2,19 +2,32 @@ import '../css/jquery.datatables.min.css';
 import React from 'react';
 
 const $ = require('jquery'); 
-$.DataTable = require('datatables.net'); 
+require('datatables.net-buttons')(window, $); 
+$.DataTable = require('datatables.net-responsive'); 
 //https://www.youtube.com/watch?v=ZCKj0SJRTB8
+//https://stackoverflow.com/questions/16539578/datatables-warning-requested-unknown-parameter-0-from-the-data-source-for-row#33639519
 export default class PokemonTable extends React.Component {
     componentDidMount() {
       this.$el = $(this.el);
       
       this.$el.DataTable(
-          {
-              responsive: true,
+          {           
+              responsive: {
+                  details: {
+                    type: "inline",
+                    display: $.fn.dataTable.Responsive.display.childRow
+                  }
+              },
+              "columnDefs": [{
+                "defaultContent": "-",
+                "targets": "_all"
+              }],
               ajax: {url:'http://localhost:3000/pokemon', dataSrc: "" },
               columns:[
                   {data: 'idNumber', title: "ID Number"},                  
-                  {data: 'name', title: "Name"},
+                  {data: 'name', title: "Name", render: (data, type, full, meta)=>{
+                      return "<a href=www.google.com>"+data+"</a>"
+                  }},
                   {data: 'kms', title: "KM/Candy"},
                   {data: 'miles', title: "Miles/Candy"},
                   {data: 'threeStars', title: "Three Stars"},
@@ -36,7 +49,7 @@ export default class PokemonTable extends React.Component {
     render() {       
 
         return (
-           <table className="display" width="100%" ref={el => this.el = el}>
+           <table className="display responsive nowrap" width="100%" ref={el => this.el = el}>
            </table>
         )
     }
