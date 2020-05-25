48f13d88d47bf080eb68266ef22160aeea72cea4
jutt <jutt@iupui.edu>
2020-05-25 16:59:32 -0400

    set secret in env vars and check for settings file

--- server/middleware.js
+++ server/middleware.js
@@ -1,11 +1,21 @@
 let jwt = require('jsonwebtoken');
-const config = require('../settings.json');
+const fs = require('fs'); 
+const path = require('path'); 
+const settingsPath = path.resolve(__dirname, '../settings.json'); 
+var secret = ""; 
+if(fs.existsSync(settingsPath)){
+  const settings = require('../settings.json'); 
+  secret = settings.secret; 
+}
+else{
+  secret = process.env.secret;
+}
 
 let checkToken = (req, res, next) => {
   let token = req.cookies.jwt; // Express headers are auto converted to lowercase
 
   if (token) {
-    jwt.verify(token, config.secret, (err, decoded) => {
+    jwt.verify(token, secret, (err, decoded) => {
       if (err) {
           return res.redirect('/login?errorCode=1'); 
       
